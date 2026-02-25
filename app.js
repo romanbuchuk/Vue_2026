98e8d046c7d70e1f055378ec56da401ab2cb54bd
Roman Buchuk <roman.buchuk@sigmatechnology.com>
2026-02-25 15:02:59 +0200

    Vue basics final

--- app.js
+++ app.js
@@ -1,36 +1,54 @@
-// const $buttonEl = document.querySelector('button');
-// const $inputEl = document.querySelector('#task');
-// const $listEl = document.querySelector('ul');
-
-// function handleClick() {
-//     const enteredValue = $inputEl.value;
-//     const $listItemEl = document.createElement('li');
-
-//     $listItemEl.textContent = enteredValue;
-//     $listEl.appendChild($listItemEl);
-
-//     $inputEl.value = '';
-// }
-
-// $buttonEl.addEventListener('click', handleClick);
 const { createApp } = Vue;
 
 const app = createApp({
     data() {
       return {
+        isBold: false,
         firstName: '',
         lastName: '',
+        contacts: [],
       }
     },
     computed: {
         fullName() {
           return `${this.firstName} ${this.lastName}`;
+        },
+        firstNameClass() {
+          //  return [{ bold: this.isBold }, { error: !this.firstName }];
+          return {
+            bold: this.isBold,
+            error: !this.firstName
+          }
+        },
+        lastNameClass() {
+          //  return [{ bold: this.isBold }, { error: !this.firstName }];
+          return {
+            bold: this.isBold,
+            error: !this.lastName
+          }
         }
     },
+    watch: {
+        // firstName(newFirstName, oldFirstName) {
+        //     console.log(`firstName changed from ${oldFirstName} to ${newFirstName}`);
+        //     this.fullName = `${newFirstName} ${this.lastName}`;
+        // },
+        // lastName(newLastName) {
+        //     this.fullName = `${this.firstName} ${newLastName}`;
+        // }
+    },
     methods: {
+        add() {
+          this.contacts.push(this.fullName);
+          this.reset();
+        },
+        remove(index) {
+          this.contacts.splice(index, 1);
+        },
         reset() {
           this.firstName = '';
           this.lastName = '';
+          this.fullName = '';
         }
     }
 });
